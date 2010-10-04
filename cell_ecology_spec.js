8fbadf4607ca35de470d6d00b2157aae8dfd4e64
Evan Light <evan.light@tripledogdare.net>
2010-10-04 00:21:06 -0400

    Refactored spec for brevity of reading; skirting the edge of 'over-macroing'; this feels better now.

--- cell_ecology_spec.js
+++ cell_ecology_spec.js
@@ -1,23 +1,6 @@
 var ecology;
 
-function cell_ecology(args) {
-  var num_neighbors = args.with_living_neighbors;
-  var predicate = args.should_be;
-
-  describe("with " + num_neighbors + " living neighbors", function() {
-    beforeEach(function() { 
-      _(num_neighbors).times(function() {
-        ecology.neighbors.push(CellEcology({cell_alive: true}));
-      });
-    });
-
-    it("should be " + predicate, function() {
-      expect(ecology["is_" + predicate]()).toEqual(true);
-    });
-  });
-}
-
-describe("A cell ecology containing a living cell", function() {
+describe("A cell ecology", function() {
   beforeEach(function() {
     ecology = CellEcology({cell_alive: true});
   });
@@ -52,36 +35,32 @@ describe("A cell ecology containing a living cell", function() {
       });
     });
   });
+})
+
+describe("A cell ecology containing a living cell", function() {
+  beforeEach(function() {
+    ecology = CellEcology({cell_alive: true});
+  });
 
   cell_ecology({with_living_neighbors:1, should_be: "underpopulated"});
   cell_ecology({with_living_neighbors:2, should_be: "life_sustaining"});
   cell_ecology({with_living_neighbors:3, should_be: "life_generating"});
   cell_ecology({with_living_neighbors:4, should_be: "overcrowded"});
 
-  live_predicates = ["is_life_sustaining", "is_life_generating"];
-  $.each(live_predicates, function(idx, predicate) {
-    var ecology_state = predicate;
-    describe("where the ecology " + ecology_state, function() {
-      beforeEach(function() {
-        ecology[ecology_state] = function() { return true };
-      });
-
-      it("should have a living cell after transitioning", function() {
-        expect(ecology.next_state()).toEqual(true);
-      });
-    });
-  });
-
+  next_cell_state({where_ecology:"is_life_sustaining", should_be: "alive"});
+  next_cell_state({where_ecology:"is_life_generating", should_be: "alive"});
 });
 
 describe("A cell ecology containing a dead cell", function() {
-  var ecology;
   beforeEach(function() {
     ecology = CellEcology({cell_alive: false});
   });
 
   cell_ecology({with_living_neighbors: 2, should_be: "life_sustaining"});
   cell_ecology({with_living_neighbors: 3, should_be: "life_generating"});
+
+  next_cell_state({where_ecology: "is_under_populated", should_be: "dead"});
+  next_cell_state({where_ecology: "is_life_sustaining", should_be: "dead"});
 
   describe("where the ecology is life generating", function() {
     beforeEach(function() {
@@ -92,18 +71,37 @@ describe("A cell ecology containing a dead cell", function() {
       expect(ecology.next_state()).toEqual(true);
     });
   });
+});
 
-  dead_predicates = ["is_under_populated", "is_life_sustaining"], 
-  $.each(dead_predicates, function(idx, predicate) {
-    var ecology_state = predicate;
-    describe("where the ecology " + ecology_state, function() {
-      beforeEach(function() {
-        ecology[ecology_state] = function() { return true };
-      });
+function cell_ecology(args) {
+  var num_neighbors = args.with_living_neighbors;
+  var predicate = args.should_be;
 
-      it("should have a dead cell after transtioning", function() {
-        expect(ecology.next_state()).toEqual(false);
+  describe("with " + num_neighbors + " living neighbors", function() {
+    beforeEach(function() { 
+      _(num_neighbors).times(function() {
+        ecology.neighbors.push(CellEcology({cell_alive: true}));
       });
     });
+
+    it("should be " + predicate, function() {
+      expect(ecology["is_" + predicate]()).toEqual(true);
+    });
   });
-});
+}
+
+function next_cell_state(args) {
+  var ecology_state = args.where_ecology
+  var expected_cell_state = args.should_be == "alive";
+  var life_adjective = expected_cell_state ? "living" : "dead";
+
+  describe("where the ecology " + ecology_state, function() {
+    beforeEach(function() {
+      ecology[ecology_state] = function() { return true };
+    });
+
+    it("should have a " + life_adjective + " cell after transitioning", function() {
+      expect(ecology.next_state()).toEqual(expected_cell_state);
+    });
+  });
+}
