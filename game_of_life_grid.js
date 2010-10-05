3788acec8eab88d027d86e81285bbd7b42349785
Evan Light <evan.light@tripledogdare.net>
2010-10-04 22:58:32 -0400

    Now each grid member needs to have neighbors

--- game_of_life_grid.js
+++ game_of_life_grid.js
@@ -1,10 +1,12 @@
 var GameOfLifeGrid = function(board) {
-  if (board.length != board[0].length) {
+  var board_size = board.length;
+  if (board_size != board[0].length) {
     throw "M x N not allowed";
   }
   return {
-    size: function() { return board.length; },
+    size: function() { return board_size; },
     state: function() { return board; },
+    get: function() { return CellEcology({alive: true}) },
     iterate: function() { 
       board = [
         [1, 1],
--- grid_ecology_spec.js
+++ grid_ecology_spec.js
@@ -5,10 +5,10 @@ describe("A Game of Life grid of N x N dimensions", function() {
       [1, 1],
       [1, 0]
     ];
+    grid = GameOfLifeGrid(input_state);
   });
 
   it("should expect a board of N x N as input_state", function() {
-    grid = GameOfLifeGrid(input_state);
     expect(grid).toBeDefined();
   });
 
@@ -20,28 +20,61 @@ describe("A Game of Life grid of N x N dimensions", function() {
   });
 
   it("should know that it is of size N", function() {
-    grid = GameOfLifeGrid(input_state);
     expect(grid.size()).toEqual(input_state.length);
   });
 
   it("should be able to extract its current state", function() {
-    grid = GameOfLifeGrid(input_state);
     expect(grid.state()).toEqual(input_state);
+  });
+
+  it("should be able to get the CellEcology at any position in the grid", function() {
+    expect(grid.get(0, 0)).toBeDefined();
+    expect(grid.get(1, 1)).toBeDefined();
   });
 });
 
 describe("A 2 x 2 game of life", function() {
-  it("should be able to transition to its next state", function() {
-    var input_state = [
+  var input_state, expected_state;
+  beforeEach(function() {
+    input_state = [
       [1, 1],
       [1, 0]
     ];
-    var expected_state = [
+    expected_state = [
       [1, 1],
       [1, 1]
+    ];
+  });
+
+  it("should be able to transition to its next state", function() {
+    grid = GameOfLifeGrid(input_state);
+    grid.iterate();
+    expect(grid.state()).toEqual(expected_state);
+  });
+
+  it("should assign neighbors to each cell when the grid is created", function() {
+    grid = GameOfLifeGrid(input_state);
+    expect(grid.get(0, 0).neighbors.length).toEqual(3);
+    expect(grid.get(0, 0).num_living_neighbors()).toEqual(2);
+  });
+});
+
+describe("A 3 x 3 game of life", function() {
+  it("should be able to transition to its next state", function() {
+    var input_state = [
+      [1, 1, 1],
+      [1, 0, 1],
+      [1, 1, 1]
+    ];
+    var expected_state = [
+      [1, 0, 1],
+      [0, 0, 0],
+      [1, 0, 1]
     ];
     grid = GameOfLifeGrid(input_state);
     grid.iterate();
     expect(grid.state()).toEqual(expected_state);
   });
 });
+
+
